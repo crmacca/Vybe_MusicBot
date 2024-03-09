ee7af54b22b1587d7959fc76bbda6bfc246d3869
Christian <55185704+crmacca@users.noreply.github.com>
2024-03-09 12:18:33 +1100

    Delete utilities/configuration.js

--- utilities/configuration.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    discord: {
-        token: 'MTEzNTgzMjYyMTg0MzIzMDc0MA.GHeNme.mQ8A-w0zTN164h7OfCVRuOmcq2tkOgw7yELlx8',
-        guildID: '1137621836813840435'
-    },
-    apiKeys: {
-        soundCloud: 'tYCjXNdxZd4oVYgrpHjo5kaQmpQnRAvs',
-    },
-}
